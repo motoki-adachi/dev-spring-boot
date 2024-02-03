5b7bacb810599354bb9e9c64e796f6240570843b
motoki-adachi <59963342+motoki-adachi@users.noreply.github.com>
2024-02-03 13:00:34 +0900

    dev

--- mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,17 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
 
     @GetMapping("/")
     public String sayHello() {
@@ -19,5 +26,10 @@ public class FunRestController {
     @GetMapping("/fortune")
     public String getDailyFortune(){
         return "Today is your lucky day.";
+    }
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
     }
 }
