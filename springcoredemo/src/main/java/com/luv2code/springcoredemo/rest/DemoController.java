802d244c27b8ea28e62222454877e36fa2ffc7af
motoki-adachi <59963342+motoki-adachi@users.noreply.github.com>
2024-02-05 20:29:14 +0900

    dev

--- springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.luv2code.springcoredemo.rest;
 
 import com.luv2code.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,14 +11,8 @@ public class DemoController {
 
     private Coach myCoach;
 
-//    // setter injection
-//    @Autowired
-//    public void doSomeStuff(Coach theCoach) {
-//        this.myCoach = theCoach;
-//    }
-
     @Autowired
-    public DemoController(Coach theCoach) {
+    public DemoController(@Qualifier("tennisCoach") Coach theCoach) {
         this.myCoach = theCoach;
     }
 
