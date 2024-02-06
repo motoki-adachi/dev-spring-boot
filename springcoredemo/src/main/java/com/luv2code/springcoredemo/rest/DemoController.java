14285f5116a6f479fa163706f145dfc674abd67b
motoki-adachi <59963342+motoki-adachi@users.noreply.github.com>
2024-02-06 20:35:11 +0900

    dev

--- springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -10,15 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     private Coach myCoach;
+    private Coach anotherCoach;
 
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+    public DemoController(
+            @Qualifier("cricketCoach") Coach theCoach,
+            @Qualifier("cricketCoach") Coach theAnotherCoach) {
         System.out.println("In constructor: " + getClass().getSimpleName());
         this.myCoach = theCoach;
+        this.anotherCoach = theAnotherCoach;
     }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
+    }
+    
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
     }
 }
