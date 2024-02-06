f3689738051cd5fd7f0baeda7092c57b19464ea4
motoki-adachi <59963342+motoki-adachi@users.noreply.github.com>
2024-02-06 20:24:28 +0900

    dev

--- springcoredemo/src/main/java/com/luv2code/springcoredemo/common/BaseballCoach.java
+++ springcoredemo/src/main/java/com/luv2code/springcoredemo/common/BaseballCoach.java
@@ -1,11 +1,13 @@
 package com.luv2code.springcoredemo.common;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
 public class BaseballCoach implements Coach {
+
+    public BaseballCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
 
     @Override
     public String getDailyWorkout() {
