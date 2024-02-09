68edcfb1f9f5e2c17a4a72b8c2447748a7b737cb
motoki-adachi <zyx.wvu43@icloud.com>
2024-02-09 22:32:26 +0900

    dev

--- cruddemo/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ cruddemo/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -17,8 +17,23 @@ public class CruddemoApplication {
     @Bean
     public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
         return runner -> {
-            createStudent(studentDAO);
+            // createStudent(studentDAO);
+            createMultipleStudents(studentDAO);
         };
+    }
+
+    private void createMultipleStudents(StudentDAO studentDAO) {
+
+        System.out.println("Creating 3 student object ...");
+        Student tempStudent1 = new Student("Bonita", "Doe", "bonita@luv2code.com");
+        Student tempStudent2 = new Student("Paul", "Doe", "paul@luv2code.com");
+        Student tempStudent3 = new Student("Mike", "Doe", "mike@luv2code.com");
+
+        System.out.println("Saving the students ...");
+        studentDAO.save(tempStudent1);
+        studentDAO.save(tempStudent2);
+        studentDAO.save(tempStudent3);
+
     }
 
     private void createStudent(StudentDAO studentDAO) {
