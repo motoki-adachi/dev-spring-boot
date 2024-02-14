1391ee84376cbd0fd397f5e3533a290718172d21
motoki-adachi <zyx.wvu43@icloud.com>
2024-02-14 21:09:18 +0900

    dev

--- springcruddemo/src/main/java/com/luv2code/springboot/springcruddemo/rest/EmployeeRestController.java
+++ springcruddemo/src/main/java/com/luv2code/springboot/springcruddemo/rest/EmployeeRestController.java
@@ -36,6 +36,22 @@ public class EmployeeRestController {
         Employee dbEmployee = employeeService.save(theEmployee);
         return dbEmployee;
     }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+        Employee tempEmployee = employeeService.findById(employeeId);
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        employeeService.deleteById(employeeId);
+        return "Deleted employee id - " + employeeId;
+    }
 }
 
 
