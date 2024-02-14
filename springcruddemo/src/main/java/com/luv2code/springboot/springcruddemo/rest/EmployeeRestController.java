9e9c8b6d8af4ed530b6305e29527331eb7aff3f9
motoki-adachi <zyx.wvu43@icloud.com>
2024-02-14 21:00:44 +0900

    dev

--- springcruddemo/src/main/java/com/luv2code/springboot/springcruddemo/rest/EmployeeRestController.java
+++ springcruddemo/src/main/java/com/luv2code/springboot/springcruddemo/rest/EmployeeRestController.java
@@ -2,9 +2,7 @@ package com.luv2code.springboot.springcruddemo.rest;
 
 import com.luv2code.springboot.springcruddemo.entity.Employee;
 import com.luv2code.springboot.springcruddemo.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,4 +20,36 @@ public class EmployeeRestController {
     public List<Employee> findAll() {
         return employeeService.findAll();
     }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        return theEmployee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+        theEmployee.setId(0);
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
