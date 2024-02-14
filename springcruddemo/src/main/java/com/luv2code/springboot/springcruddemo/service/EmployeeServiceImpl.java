b0d363795e2ef4fd8e0016c1b4f7d34cb6d20055
motoki-adachi <zyx.wvu43@icloud.com>
2024-02-14 20:45:54 +0900

    dev

--- springcruddemo/src/main/java/com/luv2code/springboot/springcruddemo/service/EmployeeServiceImpl.java
+++ springcruddemo/src/main/java/com/luv2code/springboot/springcruddemo/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.springcruddemo.service;
 import com.luv2code.springboot.springcruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.springcruddemo.entity.Employee;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,5 +19,22 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    @Override
+    @Transactional
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
     }
 }
