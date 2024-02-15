d9a99174963ed198f1f4336682cbc4895785af4d
motoki-adachi <zyx.wvu43@icloud.com>
2024-02-15 21:15:54 +0900

    dev

--- springcruddemo/src/main/java/com/luv2code/springboot/springcruddemo/dao/EmployeeRepository.java
+++ springcruddemo/src/main/java/com/luv2code/springboot/springcruddemo/dao/EmployeeRepository.java
@@ -3,5 +3,6 @@ package com.luv2code.springboot.springcruddemo.dao;
 import com.luv2code.springboot.springcruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// @RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
