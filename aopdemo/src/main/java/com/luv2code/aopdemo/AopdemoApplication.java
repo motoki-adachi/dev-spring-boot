97df77af6d8cce0d82b242b12efd77baeeeffb33
motoki-adachi <zyx.wvu43@icloud.com>
2024-02-19 19:59:14 +0900

    dev

--- aopdemo/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
+++ aopdemo/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
@@ -1,7 +1,9 @@
 package com.luv2code.aopdemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AopdemoApplication {
@@ -10,4 +12,10 @@ public class AopdemoApplication {
 		SpringApplication.run(AopdemoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String args[]) {
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
 }
